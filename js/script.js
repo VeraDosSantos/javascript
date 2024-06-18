df719c861637943b3983243df4fb58f6fd05623d
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-06-18 10:16:00 +0200

    fin du cours 2

--- js/script.js
+++ js/script.js
@@ -8,13 +8,13 @@
 */
 
 // -- 1 : Déclarer une variable en JS
-let Prenom;
+let prenom;
 
 // -- 2 : Affecter une valeur à une variable
-Prenom = "Mohamed";
-console.log(Prenom);
+prenom = "Mohamed";
+//console.log(Prenom);
 
-Prenom = "Marwan";
+prenom = "Marwan";
 
 // -- 3 : une instruction se termine TOUJOURS par un point-virgule, aussi il est possible d'écrire plusieurs instructions sur une seule ligne
 // inst_1;
@@ -25,10 +25,10 @@ Prenom = "Marwan";
 // window.alert("Super, tu es arrivé sur mon site !");
 
 // -- 5 : Afficher dans la console (ici, la valeur de ma variable Prenom)
-console.log(Prenom);
+//console.log(Prenom);
 
 // -- 6 : Afficher dans la page web
-document.write("<h1>Hello</h1>");
+//document.write("<h1>Hello</h1>");
 
 // -- 7 : Demander à l'utilisateur une valeur (2 façons)
 // prompt("Question: on est quel jour ?", "jour de la semaine");
@@ -36,9 +36,9 @@ document.write("<h1>Hello</h1>");
 
 // -- et pour manipuler cette valeur, je n'oublie pas de la stocker
 let jour;
-jour = prompt("Question: on est quel jour ?", "jour de la semaine");
+//jour = prompt("Question: on est quel jour ?", "jour de la semaine");
 //let jour = prompt("Question: on est quel jour ?", "jour de la semaine");
-console.log(jour);
+//console.log(jour);
 
 // -- 8 : Attention /!\ le JS est sensible à la casse ('case sensitive')
 // mavariable =/= maVariable =/= ma_variable
@@ -63,3 +63,47 @@ let nombre_a_virgule = -5.32;
 let unBooleen = false; // -- true
 
 // -- 5. Les Constantes
+
+/* La déclaration const permet de créer une constante accessible uniquement en lecture. Contrairement à une variable sa valeur ne peut plus être modifiée par réaffectation plus bas dans le code.
+ Une constante ne peut pas être déclarée à nouveau dans le même script.
+*/
+
+// -- Par convention les constantes sont en majuscules
+const PAYS = "France"; // string
+const AN = '2020'; // string
+const BIRTH = 2020; // nombre
+
+// -- 6. typeof permet de connaître le type de ma variable
+//console.log(annee);
+//console.log(typeof annee);
+//console.log(vacances);
+//console.log(typeof vacances + " c'est le type de : " + vacances);
+
+
+let unChiffre = "12";
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+// STRING => NUMBER
+unChiffre = parseInt(unChiffre);
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+// je ré-affecte une valeur
+unChiffre = "12.22";
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+// STRING => FLOAT
+unChiffre = parseFloat(unChiffre);
+console.log(unChiffre);
+console.log(typeof unChiffre);
+
+// NUMBER => STRING
+let nb_en_lettres = 258;
+console.log(nb_en_lettres);
+console.log(typeof nb_en_lettres);
+
+nb_en_lettres = nb_en_lettres.toString();
+console.log(nb_en_lettres);
+console.log(typeof nb_en_lettres);
